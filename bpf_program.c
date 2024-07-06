444e4483567e37849e69d5e493c85c6fe89d6956
Lev <gaz358@Home-PC.>
2024-07-06 15:00:12 +0300

    bpf

--- bpf_program.c
+++ bpf_program.c
@@ -1,10 +1,14 @@
 #include <linux/bpf.h>
-#define SEC(NAME) __attribute__((section(NAME), used))
+#include <bpf/bpf_helpers.h>
+
+// Удаляем собственное определение макроса SEC
+
 SEC("tracepoint/syscalls/sys_enter_execve")
 int bpf_prog(void *ctx)
 {
     char msg[] = "Hello, BPF World!";
-    bpf_trace_printk(msg, sizeof(msg));
+    bpf_printk("%s\n", msg);
     return 0;
 }
+
 char _license[] SEC("license") = "GPL";
