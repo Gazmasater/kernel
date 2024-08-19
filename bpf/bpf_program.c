bb0770e2bbf0e11c00b9269862cbfad35ce918d0
Gazmasater <tolkoplus@gmail.com>
2024-08-20 00:15:24 +0300

    headers installed

--- bpf/bpf_program.c
+++ bpf/bpf_program.c
@@ -1,41 +1,14 @@
 #include <linux/bpf.h>
-#include <linux/if_ether.h>
-#include <linux/ip.h>
-#include <linux/udp.h>
 #include <bpf/bpf_helpers.h>
 
-#define IPPROTO_UDP 17
-
 SEC("xdp")
-int xdp_prog(struct xdp_md *ctx) {
-    void *data = (void *)(long)ctx->data;
-    void *data_end = (void *)(long)ctx->data_end;
-   
-    struct ethhdr *eth = data;
-    if ((void *)(eth + 1) > data_end)
-        return XDP_DROP;
-
-    if (eth->h_proto != __constant_htons(ETH_P_IP))
-        return XDP_PASS;
-
-    struct iphdr *ip = (void *)(eth + 1);
-    if ((void *)(ip + 1) > data_end)
-        return XDP_DROP;
-
-    if (ip->protocol != IPPROTO_UDP)
-        return XDP_PASS;
-
-    struct udphdr *udp = (void *)(ip + 1);
-    if ((void *)(udp + 1) > data_end)
-        return XDP_DROP;
-
-    bpf_printk("UDP src port: %d, dest port: %d\n",
-               __constant_ntohs(udp->source), __constant_ntohs(udp->dest));
-
+int xdp_prog(struct __sk_buff *skb) {
     return XDP_PASS;
 }
 
 char _license[] SEC("license") = "GPL";
+
+
 
 
 	
